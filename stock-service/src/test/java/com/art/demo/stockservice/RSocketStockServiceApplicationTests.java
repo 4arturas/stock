72468ad07c438863f950fa4efafba3cf77fa540f
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2021-01-08 19:41:25 +0200

    Add RSocketStockServiceApplicationTests

---
+++ stock-service/src/test/java/com/art/demo/stockservice/RSocketStockServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.art.demo.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RSocketStockServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
